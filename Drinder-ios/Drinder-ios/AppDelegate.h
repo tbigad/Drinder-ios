5c34319f83bc868714260de48eb6480be9fb2ddb
Pavel Nadolski <tbigad@gmail.com>
2020-07-31 21:28:10 +0300

    init project

---
+++ Drinder-ios/Drinder-ios/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Drinder-ios
+//
+//  Created by Pavel N on 7/31/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, retain)UIWindow* window;
+@end
+
---
+++ Drinder-ios/Drinder-ios/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Drinder-ios
+//
+//  Created by Pavel N on 7/31/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
