7721461ac6784381e33d65942a5d5b008b9149e2
yuliya <penguindobro@gmail.com>
2020-08-01 14:46:40 +0300

    add VC

---
+++ Drinder-ios/Drinder-ios/UI/More/MoreViewController.h
@@ -0,0 +1,17 @@
+//
+//  MoreViewController.h
+//  Drinder-ios
+//
+//  Created by Юлия Лопатина on 8/1/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoreViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Drinder-ios/Drinder-ios/UI/More/MoreViewController.m
@@ -0,0 +1,32 @@
+//
+//  MoreViewController.m
+//  Drinder-ios
+//
+//  Created by Юлия Лопатина on 8/1/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import "MoreViewController.h"
+
+@interface MoreViewController ()
+
+@end
+
+@implementation MoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
