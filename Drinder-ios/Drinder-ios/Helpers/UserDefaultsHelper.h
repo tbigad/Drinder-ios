c6915055d97381a616d76ede76f5586f0b30adf2
Pavel Nadolski <tbigad@gmail.com>
2020-08-01 13:50:04 +0300

    add helper

---
+++ Drinder-ios/Drinder-ios/Helpers/UserDefaultsHelper.h
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsHelper.h
+//  Drinder-ios
+//
+//  Created by Pavel N on 8/1/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserDefaultsHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Drinder-ios/Drinder-ios/Helpers/UserDefaultsHelper.m
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsHelper.m
+//  Drinder-ios
+//
+//  Created by Pavel N on 8/1/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import "UserDefaultsHelper.h"
+
+@implementation UserDefaultsHelper
+
+@end
