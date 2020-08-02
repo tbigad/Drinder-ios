0d5c75e362b36a4d7fa3777d4ce5b66b5be3cd62
yuliyalop <penguindobro@gmail.com>
2020-08-03 00:21:00 +0300

    add detailVC

---
+++ Drinder-ios/Drinder-ios/UI/Detail/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Drinder-ios
+//
+//  Created by Юлия Лопатина on 8/2/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Drinder-ios/Drinder-ios/UI/Detail/DetailViewController.m
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.m
+//  Drinder-ios
+//
+//  Created by Юлия Лопатина on 8/2/20.
+//  Copyright © 2020 Pavel N. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem new];
+    self.navigationItem.leftBarButtonItem.title = @"Save";
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
